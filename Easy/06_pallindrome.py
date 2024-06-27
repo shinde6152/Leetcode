8df3204553cf8850a0acccf978930a06304ad3e1
shinde6153 <165893316+shinde6153@users.noreply.github.com>
2024-06-27 11:17:31 +0530

    check int pallindrome or not

--- Easy/06_pallindrome.py
+++ Easy/06_pallindrome.py
@@ -3,15 +3,15 @@
 
 
 
-def rev_int(num):
+def rev_int(num):                           #       function to reverse numbers
     reverser_num = int(str(num)[::-1])
     return reverser_num
 
-def check_pallindrome(num):
+
+def check_pallindrome(num):                 #       Function to check pallindrome
     num2 = rev_int(num)
     if num == num2:
         print("number is pallindrome",num)
-        return check_pallindrome(int(input()))
     else:
         print("Enter correct pallindrome")
         return check_pallindrome(int(input()))
